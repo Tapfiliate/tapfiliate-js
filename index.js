83c53fb2ab85b939c407d885d2ee981b5ce3095c
C. Hodor <120195957+Cosmin-Hodor@users.noreply.github.com>
2024-02-07 12:34:24 +0200

    Updating code to Es6 and fixing "this.src" reading context (Avoid using "this" in arrow functions)

--- index.js
+++ index.js
@@ -1,30 +1,20 @@
 const handler = {
-  get: function (target, name) {
-    return target.hasOwnProperty(name)
-      ? target[name]
-      : (...args) => {
-          window.__tap(name, ...args);
-        };
+  get: function(target, name) {
+    return target.hasOwnProperty(name) ? target[name] : (...args) => {
+      window.__tap(name, ...args);
+    };
   }
 };
 
 const target = {
-  init: (
-    accountId,
-    createOptions = { integration: "npm-module" },
-    createCallback,
-    detectOptions = {},
-    detectCallback
-  ) => {
+  init: (accountId, createOptions = { integration: "npm-module" }, createCallback, detectOptions = {}, detectCallback) => {
     if (window.__tap) return;
 
-    (function (t, a, p) {
+    (function(t, a, p) {
       t.TapfiliateObject = a;
-      t[a] =
-        t[a] ||
-        function () {
-          (t[a].q = t[a].q || []).push(arguments);
-        };
+      t[a] = t[a] || function() {
+        (t[a].q = t[a].q || []).push(arguments);
+      };
     })(window, "__tap");
 
     var script = document.createElement("script");
@@ -33,14 +23,16 @@ const target = {
     script.async = true;
 
     document.getElementsByTagName("head")[0].appendChild(script);
-    script.addEventListener("error", () => {
-      new Error(`${this.src} failed to load.`);
+
+    script.addEventListener("error", function() {
+      throw new Error(`${this.src} failed to load.`);
     });
 
     window.__tap("create", accountId, createOptions, createCallback);
     window.__tap("detect", detectOptions, detectCallback);
   }
 };
+
 var Tap = new Proxy(target, handler);
 
 export default Tap;
